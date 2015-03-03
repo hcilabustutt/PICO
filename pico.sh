da8326912b3608f2b2ac16146bf5939763f50b9d
Jeremy Schwartz <jeremy@worldpossible.org>
2015-03-03 15:01:57 -0800

    home

--- pico.sh
+++ pico.sh
@@ -5,6 +5,7 @@ sudo apt-get install minicom
 minicom -b 38400 -o -D /dev/ttyAMA0
 #press UPSR to see minicom welcome screen
 sudo cp raspi-blacklist.conf /etc/modprobe.d/raspi-blacklist.conf
+sudo cp picofssd.py /home/pi/picofssd.py
 sudo cp config.txt /boot/config.txt
 sudo cp modules /etc/modules
 sudo reboot
