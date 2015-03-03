5f0282054631a66a5a866384c12eee45375b60ab
Jeremy Schwartz <jeremy@worldpossible.org>
2015-03-03 12:36:34 -0800

    modules

--- pico.sh
+++ pico.sh
@@ -4,4 +4,5 @@ minicom -b 38400 -o -D /dev/ttyAMA0
 #press UPSR to see minicom welcome screen
 sudo cp raspi-blacklist.conf /etc/modprobe.d/raspi-blacklist.conf
 sudo cp config.txt /boot/config.txt
+sudo cp modules /etc/modules
 sudo reboot
