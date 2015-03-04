5ed60a1acb349a47a45f3a0e7366eb538e8977a8
Jeremy Schwartz <jeremy@worldpossible.org>
2015-03-03 16:16:06 -0800

    bootloader

--- pico2.sh
+++ pico2.sh
@@ -6,6 +6,9 @@ exit
 sudo hwclock -r
 sudo hwclock -w
 sudo cp rc.local /etc/rc.local
+sudo cp UPS_PIco.hex /home/pi/UPS_PIco.hex
+sudo cp picofu.py /home/pi/picofu.py
+cd ..
 sudo i2cset -y 1 0x6b 0x00 0xff && python picofu.py -f UPS_PIco.hex
 sudo cp picofssd.py /home/pi/picofssd.py
 sudo python /home/pi/picofssd.py
